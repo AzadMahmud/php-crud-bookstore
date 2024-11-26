ee351f763ef46fe9a08c942890bbb2c58a706199
Azad Mahmud <azadmahmud2k@gmail.com>
2024-11-27 04:02:39 +0600

    added index view

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Book;
 use Illuminate\Database\Seeder;
+use \App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,12 +14,16 @@ class DatabaseSeeder extends Seeder
      * Seed the application's database.
      */
     public function run(): void
+
     {
-        // \App\Models\User::factory(10)->create();
+        User::truncate();
+        User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        Book::truncate();
+        Book::factory(100)->create();
     }
 }
---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<h1>Bookstore</h1>
+<table class="table table-striped table-hover">
+  <thead>
+    <tr> 
+      <th>ID</th>
+      <th >Title</th>
+      <th >Author</th>
+      <th >ISBN</th>
+      <th>Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($books as $book)
+    <t>
+      <td> {{$book->id}} </td>
+      <td>{{$book->title}}</td>
+      <td>{{$book->author}}</td>
+      <td>{{$book->isbn}}</td>
+      <td>
+        <a href=""></a>
+      </td>
+    </t>
+    @endforeach
+  </tbody>
+</table>
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
